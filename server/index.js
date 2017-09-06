2edb1a469457afc5135876f6fbf58cfcd0d5f3c1
yonatanmk <yonatanmk@gmail.com>
2017-09-06 19:08:42 -0400

    make server heroku-ready

--- server/index.js
+++ server/index.js
@@ -5,4 +5,5 @@ app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000 // lets heroku declare which port our app will use, default of 5000
+app.listen(PORT);
