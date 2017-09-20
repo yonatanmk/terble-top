e0bfa09cb5c8887c236b95b27a0b4c6743278cd4
yonatanmk <yonatanmk@gmail.com>
2017-09-20 14:10:42 -0400

    added react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 // Rendering Layer control (e.g. React Router)
 
 import React, { Component } from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import * as actions from '../actions';
 
@@ -9,6 +9,11 @@ import React, { Component } from 'react';
 // import Landing from './Landing';
 // import Dashboard from './Dashboard';
 // import SurveyNew from './surveys/SurveyNew';
+
+const Header = () => <h2>Header</h2>
+const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
   // componentDidMount() {
@@ -18,15 +23,14 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        {/* <BrowserRouter>
-        //   <div>
-        //     <Header />
-        //     <Route exact path="/" component={Landing} />
-        //     <Route exact path="/surveys" component={Dashboard} />
-        //     <Route path="/surveys/new" component={SurveyNew} />
-        //   </div>
-        // </BrowserRouter> */}
-        Hi There!
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
