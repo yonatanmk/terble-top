175e6d1c8be329370cce0b4234417d8373cfb145
yonatanmk <yonatanmk@gmail.com>
2017-09-20 16:49:59 -0400

    installed materialize-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,12 +5,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import * as actions from '../actions';
 
-// import Header from './Header';
+import Header from './Header';
 // import Landing from './Landing';
 // import Dashboard from './Dashboard';
 // import SurveyNew from './surveys/SurveyNew';
 
-const Header = () => <h2>Header</h2>
+// const Header = () => <h2>Header</h2>
 const Landing = () => <h2>Landing</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
