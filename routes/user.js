bb898adf5268d3d6953c1bb918aa2be194c54a2c
Yonatan MK <yonatanmk@gmail.com>
2018-03-31 12:45:09 -0400

    extract fetchAndSaveGames method

--- routes/user.js
+++ routes/user.js
@@ -2,30 +2,20 @@ const mongoose = require('mongoose');
 const rp = require('request-promise');
 const parser = require('xml2json');
 
-const { saveGameFromBBG } = require('../lib/game-utils');
+const { fetchAndSaveGames } = require('../lib/game-utils');
 
 const User = mongoose.model('users'); // Dont import models, access them like this via mongoose
 
 module.exports = app => {
 	app.post('/api/add-bbg-username', (req, res) => {
     const { _id } = req.user;
-    User.findByIdAndUpdate({ _id }, { bbgUsername: req.body.bbgUsername })
+		const { bbgUsername } = req.body;
+		
+    User.findByIdAndUpdate({ _id }, { bbgUsername })
 			.then(() => User.findById({ _id }))
       .then(user => res.send(user))
       .catch(error => console.log(error));
 
-		const options = {
-			uri: `https://bgg-json.azurewebsites.net/collection/${req.body.bbgUsername}`,
-			json: true,
-		};
-
-		rp(options)
-			.then(body => {
-				body.forEach( bbgGame => saveGameFromBBG(bbgGame));
-			})
-			.catch(err => {
-				console.log('Error Fetching Game List');
-				console.log(err);
-			});
+		fetchAndSaveGames(bbgUsername);
 	});
 };
