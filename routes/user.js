740b2accb09d9000ea3c27986196e8f63f3d9790
Yonatan MK <yonatanmk@gmail.com>
2018-03-31 16:51:25 -0400

    On bbg username submit, update user with bg username and save bbg games
    When you submit your bbg username, actions happen in this order
    
    1. Query for user
    2. Add bbgUsername to user
    3. Fetch games with bbgUsername
    4.For Each game, generate a Promise that:
      a. adds game id to user’s games array
      b. calls saveGameFromBBG to save the game to the db
        i. make call to bbg to get bestPlayers
          1. if call is successful
            a. save game if there is no game
            b. update game if the game exists without bestPlayers
          2. if call fails
            a. save game with no bestPlayers
            b. do nothing if game already exists
    5. Call all save game promises with Promise.all
    6. Save new user
    7. Return new user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const rp = require('request-promise');
-const parser = require('xml2json');
 
-const { fetchAndSaveGames } = require('../lib/game-utils');
+const { saveGameFromBBG, ignoreGame } = require('../lib/game-utils');
 
 const User = mongoose.model('users'); // Dont import models, access them like this via mongoose
 
@@ -10,12 +9,34 @@ module.exports = app => {
 	app.post('/api/add-bbg-username', (req, res) => {
     const { _id } = req.user;
 		const { bbgUsername } = req.body;
-		
-    User.findByIdAndUpdate({ _id }, { bbgUsername })
-			.then(() => User.findById({ _id }))
-      .then(user => res.send(user))
-      .catch(error => console.log(error));
 
-		fetchAndSaveGames(bbgUsername);
+		User.findById({ _id })
+			.then(user => {
+				user.bbgUsername = bbgUsername;
+				const options = {
+					uri: `https://bgg-json.azurewebsites.net/collection/${bbgUsername}`,
+					json: true,
+				};
+
+				return rp(options)
+					.then(body => {
+						const promiseActions = body
+							.filter(bbgGame => !ignoreGame(bbgGame))
+							.map(bbgGame => {
+								if (!user.games.includes(bbgGame.gameId.toString())) {
+									user.games.push(bbgGame.gameId);
+								}
+
+								return saveGameFromBBG(bbgGame);
+							});
+
+						return Promise.all([Promise.resolve(user), promiseActions])
+							.then(_body => {
+								const newUser = _body[0];
+								return newUser.save();
+							})
+							.then(_user => res.send(_user));
+					});
+			});
 	});
 };
